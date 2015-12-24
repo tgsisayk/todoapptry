82e54a33179698487ab4323d6252cc9128880d9d
tgsisayk <tgsisayk@gmail.com>
2015-12-24 15:54:41 +0300

    modified:   db.sqlite3 	modified:   polls/admin.py 	modified:   polls/admin.pyc 	modified:   polls/models.py 	modified:   polls/models.pyc 	modified:   polls/models.py~

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,6 @@
+import datetime
+
+from django.utils import timezone
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
@@ -8,6 +11,8 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 @python_2_unicode_compatible
 class Choice(models.Model):
@@ -16,3 +21,5 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+
+
